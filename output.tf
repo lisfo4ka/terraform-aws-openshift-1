8d621e8eca43c57c38e995a1e6f17774a938df84
Kuzma Rezchenko <kuzma_rezchenko@epam.com>
2018-07-23 21:18:35 +0000

    add dns_name and zone_id to outputs for load balancers

--- output.tf
+++ output.tf
@@ -6,12 +6,28 @@ output "infra_lb_name" {
   value = "${aws_elb.infra.name}"
 }
 
+output "infra_lb_dns_name" {
+  value = "${aws_elb.infra.dns_name}"
+}
+
+output "infra_lb_zone_id" {
+  value = "${aws_elb.infra.zone_id}"
+}
+
 output "master_lb_arn" {
   value = "${aws_elb.master.arn}"
 }
 
 output "master_lb_name" {
   value = "${aws_elb.master.name}"
+}
+
+output "master_lb_dns_name" {
+  value = "${aws_elb.master.dns_name}"
+}
+
+output "master_lb_zone_id" {
+  value = "${aws_elb.master.zone_id}"
 }
 
 output "master_private_dns_name" {
