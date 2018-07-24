210cb34fe47ee210d64f065a289154a18e1aa72b
Olesia Vovk <olesia_ivanenko@epam.com>
2018-07-24 13:38:29 +0000

    Update outputs - seperate private and public master resources

--- outputs.tf
+++ outputs.tf
@@ -14,21 +14,33 @@ output "infra_lb_zone_id" {
   value = "${aws_elb.infra.zone_id}"
 }
 
-# try to return public resources, otherwise fall back to private
-output "master_lb_arn" {
-  value = "${coalesce(join(" ", aws_elb.master-public.*.arn), aws_elb.master.arn)}"
+# try to return public resources, otherwise fall to empty
+output "master_public_lb_arn" {
+  value = "${coalesce(join(" ", aws_elb.master-public.*.arn), "")}"
 }
 
-output "master_lb_name" {
-  value = "${coalesce(join(" ", aws_elb.master-public.*.name), aws_elb.master.name)}"
+output "master_public_lb_name" {
+  value = "${coalesce(join(" ", aws_elb.master-public.*.name), "")}"
 }
 
-output "master_lb_dns_name" {
-  value = "${coalesce(join(" ", aws_elb.master-public.*.dns_name), aws_elb.master.dns_name)}"
+output "master_public_lb_dns_name" {
+  value = "${coalesce(join(" ", aws_elb.master-public.*.dns_name), "")}"
 }
 
-output "master_lb_zone_id" {
-  value = "${coalesce(join(" ", aws_elb.master-public.*.zone_id), aws_elb.master.zone_id)}"
+output "master_public_lb_zone_id" {
+  value = "${coalesce(join(" ", aws_elb.master-public.*.zone_id), "")}"
+}
+
+output "master_private_lb_name" {
+  value = "${aws_elb.master.name}"
+}
+
+output "master_private_lb_dns_name" {
+  value = "${aws_elb.master.dns_name}"
+}
+
+output "master_private_lb_zone_id" {
+  value = "${aws_elb.master.zone_id}"
 }
 
 output "master_private_dns_name" {
