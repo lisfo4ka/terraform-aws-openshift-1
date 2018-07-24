0b648f1abb8616802f16f20c82d237c158e977c0
Olesia Vovk <olesia_ivanenko@epam.com>
2018-07-24 05:36:28 +0000

    Update inventory.tf

--- inventory.tf
+++ inventory.tf
@@ -4,8 +4,9 @@ data "template_file" "inventory" {
   template = "${file("${path.module}/resources/inventory.template.cfg")}"
 
   vars {
-    public_hostname  = "master.${var.platform_external_subdomain}"
-    public_subdomain = "${var.platform_external_subdomain}"
+    public_hostname =  "master.${var.internet_facing == "external" ? var.platform_external_subdomain : var.platform_internal_subdomain}"
+    public_subdomain = "${var.internet_facing == "external" ? var.platform_external_subdomain : var.platform_internal_subdomain}"
+
     cluster_id       = "${var.platform_name}"
     user_name        = "${var.user_name}"
   }
