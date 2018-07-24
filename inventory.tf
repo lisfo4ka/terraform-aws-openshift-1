6b6cd2b8e1fa2e6b2ce97820c09b5e326c33c03d
Olesia Vovk <olesia_ivanenko@epam.com>
2018-07-24 18:19:47 +0000

    Update filename path for local_files
    To resolve issue with absolute path in the statefile

--- inventory.tf
+++ inventory.tf
@@ -5,7 +5,7 @@ data "template_file" "inventory" {
 
   vars {
     platform_internal_subdomain = "${var.platform_internal_subdomain}"
-    master_default_subdomain = "${var.internet_facing == "external" ? format("%s.%s", var.platform_name, var.platform_external_subdomain) : var.platform_internal_subdomain}"
+    master_default_subdomain    = "${var.internet_facing == "external" ? format("%s.%s", var.platform_name, var.platform_external_subdomain) : var.platform_internal_subdomain}"
 
     cluster_id = "${var.platform_name}"
     user_name  = "${var.user_name}"
@@ -24,10 +24,23 @@ data "template_file" "ec2_ini" {
 //  Create the inventory.
 resource "local_file" "inventory" {
   content  = "${data.template_file.inventory.rendered}"
-  filename = "${path.cwd}/inventory.cfg"
+  filename = "${data.null_data_source.path-to-local-file-inventory.outputs.filename}"
 }
 
 resource "local_file" "ec2_ini" {
   content  = "${data.template_file.ec2_ini.rendered}"
-  filename = "${path.cwd}/ec2.ini"
+  filename = "${data.null_data_source.path-to-local-file-ec2_ini.outputs.filename}"
+}
+
+// Avoid absolute filename path to be written to statefile
+data "null_data_source" "path-to-local-file-inventory" {
+  inputs {
+    filename = "${substr("${path.cwd}/inventory.cfg", length(path.cwd) + 1, -1)}"
+  }
+}
+
+data "null_data_source" "path-to-local-file-ec2_ini" {
+  inputs {
+    filename = "${substr("${path.cwd}/ec2.ini", length(path.cwd) + 1, -1)}"
+  }
 }
