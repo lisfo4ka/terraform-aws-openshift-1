691daaddb412486962267171f6e90dddd77f3531
Sergiy Kulanov <Sergiy_Kulanov@epam.com>
2018-08-02 23:34:01 +0300

    Switch master node LB from ELB to ALB

--- outputs.tf
+++ outputs.tf
@@ -16,19 +16,19 @@ output "infra_lb_zone_id" {
 
 # try to return public resources, otherwise fall to empty
 output "master_public_lb_arn" {
-  value = "${coalesce(join(" ", aws_elb.master-public.*.arn), "")}"
+  value = "${coalesce(join(" ", aws_lb.master-alb.*.arn), join(" ", aws_elb.master-public.*.arn))}"
 }
 
 output "master_public_lb_name" {
-  value = "${coalesce(join(" ", aws_elb.master-public.*.name), "")}"
+  value = "${coalesce(join(" ", aws_lb.master-alb.*.name), join(" ", aws_elb.master-public.*.name))}"
 }
 
 output "master_public_lb_dns_name" {
-  value = "${coalesce(join(" ", aws_elb.master-public.*.dns_name), "")}"
+  value = "${coalesce(join(" ", aws_lb.master-alb.*.dns_name), join(" ", aws_elb.master-public.*.dns_name))}"
 }
 
 output "master_public_lb_zone_id" {
-  value = "${coalesce(join(" ", aws_elb.master-public.*.zone_id), "")}"
+  value = "${coalesce(join(" ", aws_lb.master-alb.*.zone_id), join(" ", aws_elb.master-public.*.zone_id))}"
 }
 
 output "master_private_lb_name" {
@@ -45,9 +45,4 @@ output "master_private_lb_zone_id" {
 
 output "master_private_dns_name" {
   value = "master.${var.platform_name}"
-}
-
-output "platform_private_key" {
-  sensitive = true
-  value     = "${data.tls_public_key.platform.private_key_pem}"
 }
