2cfa6a0175441c590c1abdd87c55d200b9814c7f
Alexander_Morozov <Alexander_Morozov@epam.com>
2018-08-13 15:05:49 +0300

    Add to s3_bucket policy uniq account id
    Change-Id: Ib202558363af78f97ca426e95d440d9751d3ff5d

--- s3_logs.tf
+++ s3_logs.tf
@@ -17,7 +17,7 @@ resource "aws_s3_bucket" "elb_logs" {
         "s3:PutObject"
       ],
       "Effect": "Allow",
-      "Resource": "arn:aws:s3:::${var.platform_name}-elb-logs/*",
+      "Resource": "arn:aws:s3:::${var.platform_name}-elb-logs-${data.aws_caller_identity.current.account_id}/*",
       "Principal": {
         "AWS": [
           "${data.aws_elb_service_account.main.arn}"
