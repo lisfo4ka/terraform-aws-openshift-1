af0e6f124c4fb6befff17e10c511fc135748cd35
Alexander_Morozov <Alexander_Morozov@epam.com>
2018-08-14 21:28:37 +0300

    Fix bug with subnet counts in ASG
    Change-Id: If8f9acd0ed8c466f56ceda2f54f62bfc49117fe0

--- network.tf
+++ network.tf
@@ -1,8 +1,11 @@
 # for ebs pv provisioning, node instances must residence in all subnets.
 
 locals {
-  node_scaling_subnet_ids  = "${split(",", var.compute_node_count < length(var.private_subnet_ids) ? join(",", slice(var.private_subnet_ids, 0, var.compute_node_count)) : join(",", var.private_subnet_ids))}"
-  infra_scaling_subnet_ids = "${split(",", var.infra_node_count < length(var.private_subnet_ids) ? join(",", slice(var.private_subnet_ids, 0, var.infra_node_count)) : join(",", var.private_subnet_ids))}"
+  node_subnet_ids_count = "${var.compute_node_count < length(var.private_subnet_ids) ? var.compute_node_count : length(var.private_subnet_ids)}"
+  node_scaling_subnet_ids  = "${slice(var.private_subnet_ids, 0, local.node_subnet_ids_count)}"
+
+  infra_subnet_ids_count = "${var.infra_node_count < length(var.private_subnet_ids) ? var.infra_node_count : length(var.private_subnet_ids)}"
+  infra_scaling_subnet_ids = "${slice(var.private_subnet_ids, 0, local.infra_subnet_ids_count)}"
 }
 
 output subnets {
