e8576889b48236cd16eaa94ca0d77035bd7c77b1
Roman Kovtun <Roman_Kovtun@epam.com>
2019-07-02 11:06:25 +0300

    [EPMDEDPSUP-589] EFS deployment fails if AWS region has more than 3 Availability Zones
    Change-Id: I233243e715aa613e98eeaf761f8ade0128cda950

--- efs.tf
+++ efs.tf
@@ -8,7 +8,7 @@ resource "aws_efs_file_system" "persistent_volumes" {
 data "aws_availability_zones" "az" {}
 
 resource "aws_efs_mount_target" "persistent_volumes" {
-  count           = "${var.deploy_efs ? length(data.aws_availability_zones.az.names) : 0}"
+  count           = "${var.deploy_efs ? length(var.private_subnet_ids) : 0}"
   file_system_id  = "${aws_efs_file_system.persistent_volumes.id}"
   subnet_id       = "${element(var.private_subnet_ids, count.index)}"
   security_groups = ["${coalescelist(var.cluster_internal_security_group_ids, aws_security_group.node.*.id)}"]
